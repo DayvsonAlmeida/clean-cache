ae9e2728f9447d800c1ed00f8b3b1b6257f1b330
Dayvson <dayvsonwilkson@gmail.com>
2021-04-11 20:19:03 -0300

    ensure LocalLoadPurchases does not delete empty cache

--- src/data/usecases/load-purchases/local-load-purchase.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchase.spec.ts
@@ -29,7 +29,7 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.deleteKey).toBe('purchases');
     expect(purchases).toEqual([]);
   })
-  
+
   test('Should return a list of purchases if cache is less than 3 days old', async () => {
     const currentDate = new Date();
     const timestamp = new Date(currentDate);
@@ -89,6 +89,27 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete]);
     expect(cacheStore.fetchKey).toBe('purchases');
     expect(cacheStore.deleteKey).toBe('purchases');
+    expect(purchases).toEqual([]);
+  })
+
+  test('Should return an empty list if cache is empty', async () => {
+    const currentDate = new Date();
+    const timestamp = new Date(currentDate);
+    
+    timestamp.setDate(timestamp.getDate() - 3);
+    timestamp.setSeconds(timestamp.getSeconds() + 1);
+    
+    const { cacheStore, sut } = makeSut(currentDate);
+    
+    cacheStore.fetchResult = {
+      timestamp,
+      value: []
+    }
+    
+    const purchases = await sut.loadAll();
+    
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
+    expect(cacheStore.fetchKey).toBe('purchases');
     expect(purchases).toEqual([]);
   })
 })
