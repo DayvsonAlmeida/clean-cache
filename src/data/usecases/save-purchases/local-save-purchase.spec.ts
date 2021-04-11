7253d1f216b961ce68d1e874368b92a350ae0c6c
Dayvson <dayvsonwilkson@gmail.com>
2021-04-11 18:51:09 -0300

    ensure LocalSavePurchases resolves successfully

--- src/data/usecases/save-purchases/local-save-purchase.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchase.spec.ts
@@ -34,7 +34,7 @@ describe('LocalSavePurchases', () => {
     const { cacheStore, sut } = makeSut(timestamp);
     const purchases = mockPurchases();
 
-    await sut.save(purchases);
+    const promise = sut.save(purchases);
 
     expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
     expect(cacheStore.deleteKey).toBe('purchases');
@@ -43,6 +43,7 @@ describe('LocalSavePurchases', () => {
       timestamp,
       value: purchases
     });
+    await expect(promise).resolves.toBeFalsy()
   })
 
   test('Should throw if insert throws', async () => {
